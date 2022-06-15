dce03e1edad608b2a6435b8b29f0518c16328132
Lukas <lgs20@aluno.ifal.edu.br>
2022-06-15 12:24:48 -0300

    Telas atualizadas com flatList

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,59 +1,69 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
-import {Button, TextInput, Appbar, Searchbar  } from 'react-native-paper';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity, FlatList } from 'react-native';
+//import {Button, TextInput, Appbar, Searchbar  } from 'react-native-paper';
 
-import {useNavigation} from '@react-navigation/native';
+// import { useNavigation } from '@react-navigation/native';
 
 import Shoes from '../src/components/Shoes'
 
+import produtos from '../assets/data/produtos.json';
+
 function HomeScreen({ navigation }) {
-  const navigation = useNavigation ();
-  
+  // const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      
-      <ScrollView>
+      <FlatList data={produtos} numColumns={2} renderItem={({ item }) => (
+        < Shoes img={{ uri: item.foto }} cost={item.preco}
+          cost2={item.nome} onClick={() => alert('CLICOU')}>
+        </Shoes>
+      )
+      }>
+      </FlatList >
 
-        <View style={{flexDirection: 'row', justifyContent:'space-around'}}>         
-            <Shoes img={require('../src/assets/anel.jpg')} cost='R$70,00'
-                 cost2='Anel Coração' onClick={() => alert('CLICOU')}>              
-            </Shoes>
+      {/* <ScrollView>
 
-            <Shoes img={require('../src/assets/bracelete.jpg')} cost='R$200,00'
-                 cost2='Relógio Feminino' onClick={() => alert('CLICOU')}> {/*cost 2 é o nome da propiedade text, que é o mome do produto */}             
-            </Shoes>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+          <Shoes img={require('../src/assets/anel.jpg')} cost='R$70,00'
+            cost2='Anel Coração' onClick={() => alert('CLICOU')}>
+          </Shoes>
+
+          <Shoes img={require('../src/assets/bracelete.jpg')} cost='R$200,00'
+            cost2='Relógio Feminino' onClick={() => alert('CLICOU')}>
+          </Shoes>
+        </View >
+
+        < View style={{ flexDirection: 'row', justifyContent: 'space-around' }
+        }>
+          <Shoes img={require('../src/assets/brinco-de-milhoes.jpg')} cost='R$800,00'
+            cost2='Brinco Prata' onClick={() => alert('CLICOU')}>
+          </Shoes>
+
+          <Shoes img={require('../src/assets/pulseira.jpg')} cost='R$100,00'
+            cost2='Pulseira Prata' onClick={() => alert('CLICOU')}>
+          </Shoes>
+        </View >
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+          <Shoes img={require('../src/assets/pulseira-coracao.jpg')} cost='R$150,00'
+            cost2='Pulseira Coração' onClick={() => alert('CLICOU')}>
+          </Shoes>
+
+          <Shoes img={require('../src/assets/colar-coração.png')} cost='R$250,00'
+            cost2='Colar Coração' onClick={() => alert('CLICOU')}>
+          </Shoes>
         </View>
 
-        <View style={{flexDirection: 'row', justifyContent:'space-around'}}>          
-            <Shoes img={require('../src/assets/brinco-de-milhoes.jpg')} cost='R$800,00'
-                 cost2='Brinco Prata' onClick={() => alert('CLICOU')}>           
-            </Shoes>
+      </ScrollView > */}
 
-            <Shoes img={require('../src/assets/pulseira.jpg')} cost='R$100,00'
-                cost2='Pulseira Prata' onClick={() => alert('CLICOU')}> {/* cost é a propriedade valor do produto, cost 2 é o nome da propiedade text, que é o mome do produto */}              
-            </Shoes>
-        </View>
-
-        <View style={{flexDirection: 'row', justifyContent:'space-around'}}>         
-            <Shoes img={require('../src/assets/pulseira-coracao.jpg')} cost='R$150,00'
-                 cost2='Pulseira Coração' onClick={() => alert('CLICOU')}>             
-            </Shoes>
-
-            <Shoes img={require('../src/assets/colar-coração.png')} cost='R$250,00'
-                 cost2='Colar Coração' onClick={() => alert('CLICOU')}>              
-            </Shoes>
-        </View>
-
-     </ScrollView>
-    
-    </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-    container:{
-      flex: 2,
-      width: '100%'
-    }
+  container: {
+    flex: 2,
+    width: '100%'
+  }
 })
 export default HomeScreen;
--- src/components/Shoes/index.js
+++ src/components/Shoes/index.js
@@ -3,37 +3,37 @@ import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 
 export default function Shoes(props) {
- return (
-   <TouchableOpacity style={Style.container} onPress={props.onClick}>
+  return (
+    <TouchableOpacity style={Style.container} onPress={props.onClick}>
 
-     <Image
-      source={props.img}
-      style={Style.shoesImg}
-     />
-    <Text style={Style.shoesText}>
-      {props.cost2}
-    </Text>
-    <Text style={Style.shoesTxtPreco}>
-      {props.cost}
-    </Text>
-   </TouchableOpacity>
+      <Image
+        source={props.img}
+        style={Style.shoesImg}
+      />
+      <Text style={Style.shoesText}>
+        {props.cost2}
+      </Text>
+      <Text style={Style.shoesTxtPreco}>
+        {props.cost}
+      </Text>
+    </TouchableOpacity>
   );
 }
 
 const Style = StyleSheet.create({
-    container:{
-      paddingVertical: '10%',
-      alingItems: 'center',
-      justifyContent: 'center'
-    },
-    shoesImg:{
-      width: 170,
-      height: 170
-    },
-    shoesText:{
-      fontSize:17,
-    },
-    shoesTxtPreco:{
-      fontSize: 19,
-    }
+  container: {
+    paddingVertical: '10%',
+    alingItems: 'center',
+    justifyContent: 'center'
+  },
+  shoesImg: {
+    width: 196,
+    height: 196
+  },
+  shoesText: {
+    fontSize: 17,
+  },
+  shoesTxtPreco: {
+    fontSize: 19,
+  }
 })
