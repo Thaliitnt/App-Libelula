0d6cce4886406c5d2bc302163b09934d76a8f731
Thaliitnt <tnts1@aluno.ifal.edu.br>
2022-07-05 20:28:04 -0300

    tentativa de colocar o drawer, sem sucesso

--- navigators/drawerNavigator.js
+++ navigators/drawerNavigator.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+
+import HomeScreen from '../screens/HomeScreen';
+
 const Drawer = createDrawerNavigator();
 
-export default function Drawer() {
+export default function Drawer {
     return (
         <Drawer.Navigator useLegacyImplementation>
-            <Drawer.Screen name="Feed" component={Feed} />
-            <Drawer.Screen name="Article" component={Article} />
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            {/*<Drawer.Screen name="Article" component={Article} />*/}
         </Drawer.Navigator>
     );
 }
